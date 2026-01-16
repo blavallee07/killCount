16c3e0d28b215ee3252704f40fe142608d3feaa5
Ben Lavallée <blavallee@lcs.on.ca>
2026-01-15 22:07:36 -0500

    Initial Commit

---
+++ killCount/killCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  killCountApp.swift
+//  killCount
+//
+//  Created by Benjamin Lavallee on 2026-01-15.
+//
+
+import SwiftUI
+
+@main
+struct killCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
